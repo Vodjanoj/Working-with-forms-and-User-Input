97bcb2c84443a1cda7071c0f30eb8f7daf1dcaf3
Vodjanoj <vladimirs.vodjanojs@gmail.com>
2022-03-29 18:33:43 +0300

    Bonus: Using useReducer()

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,27 +1,55 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.isTouched }; // isTouched: state.isTouched is actually not needed here, we use just current value
+  }
+  if (action.type === "BLUR") {
+    return { isTouched: true, value: state.value }; // value: state.value is actually not needed here, we use just current value
+  }
+  if (action.type === "RESET") {
+    return { isTouched: false, value: "" };
+  }
+
+  return {
+    inputStateReducer,
+  };
+};
 
 const useInput = (validValue) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [IsTouched, setIsTouched] = useState(false);
+  // const [enteredValue, setEnteredValue] = useState("");
+  // const [IsTouched, setIsTouched] = useState(false);
 
-  const valueIsValid = validValue(enteredValue);
-  const hasError = !valueIsValid && IsTouched;
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
+
+  // const valueIsValid = validValue(enteredValue);
+  const valueIsValid = validValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+    dispatch({ type: "INPUT", value: event.target.value });
+    // setEnteredValue(event.target.value);
   };
 
   const inputBlurHandler = () => {
-    setIsTouched(true);
+    // setIsTouched(true);
+    dispatch({ type: "BLUR" });
   };
 
   const reset = () => {
-    setEnteredValue("");
-    setIsTouched(false);
+    dispatch({ type: "RESET" });
   };
 
   return {
-    enteredValue: enteredValue,
+    enteredValue: inputState.value,
     isValid: valueIsValid,
     hasError: hasError,
     valueChangeHandler,
